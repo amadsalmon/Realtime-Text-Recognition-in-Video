fa8d57256e1a17b088098964e383f6b67d437951
Amad Salmon <amad.salmon@icloud.com>
2022-01-15 21:04:28 +0100

    Code formatting

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from ocr_handler import *
 
+print("Welcome to OCR video processer.")
+filename = input("ENTER FILENAME: ")
 
-
-filename = input("Welcome to OCR video processer: ENTER FILENAME ")
-
-if (os.path.isfile(filename)):  
-
-    ocr_handler = OCR_HANDLER(filename,CV2_HELPER())
+if os.path.isfile(filename):
+    ocr_handler = OCR_HANDLER(filename, CV2_HELPER())
     ocr_handler.process_frames()
     ocr_handler.assemble_video()
-    print ("OCR PROCESS FINISHED: OUT FILE =>" + ocr_handler.out_name)
+    print("OCR PROCESS FINISHED: OUT FILE =>" + ocr_handler.out_name)
 
 else:
-    print ("FILE NOT FOUND: BYE")
+    print("FILE NOT FOUND: BYE")
