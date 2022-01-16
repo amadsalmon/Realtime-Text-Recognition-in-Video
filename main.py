c7b7e042b3bf3e8163252427f09d357865a6ff8b
Arturo-00 <acalveratonin@gmail.com>
2022-01-16 17:53:17 +0100

    update main

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from ocr_handler import *
 print("Welcome to OCR video processer.")
 filename = input("ENTER FILENAME: ")
 
+ocr_type = input("ENTER OCR_MODE (WORDS/LINES): ")
+
+
 if os.path.isfile(filename):
-    ocr_handler = OCR_HANDLER(filename, CV2_HELPER())
+    ocr_handler = OCR_HANDLER(filename, CV2_HELPER(),ocr_type)
     ocr_handler.process_frames()
     ocr_handler.assemble_video()
     print("OCR PROCESS FINISHED: OUTPUT FILE => " + ocr_handler.out_name)
